02f2c2e738933a5c0b9b35951fc1f95646e26632
hudibj <hudibj@kanyun.com>
2024-06-03 20:12:13 +0800

    ADD: Base ReviewCell

---
+++ GerritJarvis/ReviewList/View/ReviewCell.swift
@@ -0,0 +1,122 @@
+//
+//  ReviewCell.swift
+//  GerritJarvis
+//
+//  Created by hudi on 2024/5/29.
+//  Copyright © 2024 Chuanren Shang. All rights reserved.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct ReviewDisplay {
+    @ObservableState
+    struct State {
+        var project: String
+        var branch: String
+        var name: String
+        var commitMessage: String
+        var avatar: NSImage?
+        var avatarUrl: String?
+        var hasNewEvent: Bool = false
+        var isMergeConflict: Bool = false
+    }
+
+    enum Action {
+        case didPressProject
+        case didPressBranch
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            // TODO: action
+            default: return .none
+            }
+        }
+    }
+}
+
+struct ReviewCell: View {
+    @State var store: StoreOf<ReviewDisplay>
+
+    var body: some View {
+        HStack {
+            VStack {
+                WebImage(url: URL(string: store.avatarUrl ?? "")) { $0.image ?? Image(nsImage: store.avatar ?? .init())
+                }
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .clipShape(Circle())
+                ZStack(alignment: .topLeading) {
+                    Text(store.name).frame(maxWidth: 40).lineLimit(2)
+                        .font(.system(size: 10))
+                        .multilineTextAlignment(.center)
+                    if store.hasNewEvent {
+                        Circle()
+                            .foregroundStyle(.red)
+                            .frame(width: 4, height: 4)
+                    }
+                }
+            }.padding(.leading)
+            ZStack(alignment: .topTrailing) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Label {
+                        Text(store.project)
+                            .font(.system(size: 10, weight: .light))
+                            .foregroundStyle(.gray)
+                    } icon: {
+                        Image(.folder)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 12, height: 12)
+                            .foregroundStyle(.gray)
+                    }.onTapGesture {
+                        store.send(.didPressProject)
+                    }.padding(.bottom, 0)
+                    Label {
+                        Text(store.branch)
+                            .font(.system(size: 10, weight: .light))
+                            .foregroundStyle(.gray)
+                    } icon: {
+                        Image(.branch)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 12, height: 12)
+                            .foregroundStyle(.gray)
+                    }.onTapGesture {
+                        store.send(.didPressBranch)
+                    }.padding(.top, 0)
+                    Spacer()
+                    Text(store.commitMessage).font(.system(size: 12)).lineLimit(2)
+                        .frame(height: 30)
+                        .foregroundStyle(.secondary)
+                }.padding(.vertical)
+                if store.isMergeConflict {
+                    Image(.conflict).resizable().frame(width: 20, height: 20).offset(y: 20)
+                }
+            }
+            Divider()
+            Rectangle().foregroundStyle(.clear).frame(width: 44)
+        }
+        .frame(width: 400, height: 66, alignment: .leading)
+        .padding(.init(top: 5, leading: 0, bottom: 5, trailing: 0))
+    }
+}
+
+#Preview {
+    ReviewCell(store: .init(initialState: ReviewDisplay.State(
+        project: "tutor-ios-embedded",
+        branch: "feature/test",
+        name: "jessy pinkman",
+        commitMessage: "ADD: new feature teststststs tststst stststststst",
+        avatarUrl: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+        hasNewEvent: false,
+        isMergeConflict: true
+    )) {
+        ReviewDisplay()
+    })
+}
